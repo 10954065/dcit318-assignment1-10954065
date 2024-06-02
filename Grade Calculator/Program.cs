fc9127e0c81eb7c7cd992b060b4e64e030dfaaf5
FRANK TEI-DEKU(10954065) <ftei-deku@st.ug.edu.gh>
2024-06-03 05:43:15 +1400

    3rd

--- Grade Calculator/Program.cs
+++ Grade Calculator/Program.cs
@@ -7,3 +7,13 @@ namespace GradeCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Grade Calculator!");
+            while (true)
+            {
+                Console.Write("Please enter your grade here, should be between 0 and 100: ");
+                string input = Console.ReadLine();
+                int grade;
+                if (int.TryParse(input, out grade) && grade >= 0 && grade <= 100)
+                {
+                    string letterGrade = GetLetterGrade(grade);
+                    Console.WriteLine($"The corresponding letter grade is: {letterGrade}");
+
